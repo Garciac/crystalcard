7685d5d1a6ca98de2704b3da9267b5455daf606b
Chris Garcia <chrisg1245@hotmail.com>
2016-10-04 14:28:39 -0700

    1st Commit

---
+++ app/src/main/java/com/example/student/crystalcard/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.student.crystalcard;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView answerText;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float acceleration;
+    private float currentAcceleration;
+    private float previousAcceleration;
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText(Predictions.get().getPrediction());
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/student/crystalcard/Predictions.java
@@ -0,0 +1,25 @@
+package com.example.student.crystalcard;
+
+public class Predictions {
+
+    private static Predictions predictions;
+    private String[] answers;
+
+    private Predictions(){
+        answers = new String[] {
+                "Your wishes will come true.",
+                "Your wishes will NEVER come true."
+        };
+    }
+
+    public static Predictions get() {
+        if(predictions == null){
+            predictions = new Predictions();
+        }
+        return predictions;
+    }
+
+    public String getPrediction(){
+        return answers[1];
+    }
+}
